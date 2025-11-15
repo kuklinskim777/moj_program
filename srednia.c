8da88ed787736f7da795212e9c47b6836a643aee
Mateusz <m.kuklinski.509@studms.ug.edu.pl>
2025-11-16 00:42:46 +0100

    Program liczacy srednia

---
+++ srednia.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    double srednia = (a + b + c) / 3.0;
+
+    printf("Srednia: %f\n", srednia);
+
+    return 0;
+}
